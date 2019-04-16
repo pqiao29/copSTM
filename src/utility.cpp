20d6a5206ec6017877705eee0888a4f0fb5a5e96
pqiao29 <pqiao@student.unimelb.edu.au>
2019-04-16 12:16:45 +1000

    Clearing std::isinf's, have trouble with server

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,9 +1,5 @@
 #include <Rcpp.h>
-using namespace Rcpp;
-#include <cmath>
-using std::isinf;
 #include <vector>
-using std::vector;
 
 #include "bvnorm.h"
 
@@ -18,7 +14,7 @@ double f_cond_cpp(const double& up, const double& down, const double& condi, con
 }
 
 double h_cpp(const double& x, const double& y, const double& rho){
-  if(isinf(x) || isinf(y)){
+  if(std::isinf(x) || std::isinf(y)){
     return 0;
   }else{
     return exp(-(x*x - 2*rho*x*y + y*y)/(2*(1 - rho*rho)));
@@ -29,13 +25,13 @@ double h_cpp(const double& x, const double& y, const double& rho){
  *  Score for main effects
  */
 
-double score_main_cpp(const vector<int>& pair_ind, const int& par_pos, const double& rho,
-                      const vector<double>& lower_bd, const vector<double>& upper_bd, 
-                      const vector<vector<double> >& upper_bdd, 
-                      const vector<vector<double> >& lower_bdd){
+double score_main_cpp(const std::vector<int>& pair_ind, const int& par_pos, const double& rho,
+                      const std::vector<double>& lower_bd, const std::vector<double>& upper_bd, 
+                      const std::vector<std::vector<double> >& upper_bdd, 
+                      const std::vector<std::vector<double> >& lower_bdd){
   int i = pair_ind[0], j = pair_ind[1];
   double a1 = lower_bd[i], a2 = lower_bd[j], b1 = upper_bd[i], b2 = upper_bd[j];
-  vector<double> bd1{a1, b1}, bd2{a2, b2};
+  std::vector<double> bd1{a1, b1}, bd2{a2, b2};
   
   double comp_lik = dbnorm(bd1, bd2, rho);
   if(!comp_lik){
@@ -58,12 +54,12 @@ double score_main_cpp(const vector<int>& pair_ind, const int& par_pos, const dou
 /*
  *  Score for correlation parameters
  */
-double score_rho_cpp(const vector<int>& pair_ind, double rho, 
-                     const vector<double>& lower_bd, const vector<double>& upper_bd, 
+double score_rho_cpp(const std::vector<int>& pair_ind, double rho, 
+                     const std::vector<double>& lower_bd, const std::vector<double>& upper_bd, 
                      const double& pi){
   int i = pair_ind[0], j = pair_ind[1];
   double a1 = lower_bd[i], a2 = lower_bd[j], b1 = upper_bd[i], b2 = upper_bd[j];
-  vector<double> bd1{a1, b1}, bd2{a2, b2};
+  std::vector<double> bd1{a1, b1}, bd2{a2, b2};
   
   double t1 = h_cpp(a1, b2, rho) + h_cpp(a2, b1, rho) - h_cpp(a1, a2, rho) - h_cpp(b1, b2, rho),
     t2 = dbnorm(bd1, bd2, rho);
